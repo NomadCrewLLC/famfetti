2734b695ada344491f42c4bd32d4fd5feb469745
Jyle Vergara <jylevergara@gmail.com>
2026-05-22 15:09:18 -0400

    add edit profile screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="profile" options={{ headerShown: true, title: 'Edit profile' }} />
         </Stack>
       </AuthGate>
     </ThemeProvider>
